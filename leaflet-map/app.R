c3909790972c19f3255c96834de006165d011f2e
Pingfan Hu <pingfan0727@gmail.com>
2024-12-07 23:35:25 -0500

    state color changes upon click

--- leaflet-map/app.R
+++ leaflet-map/app.R
@@ -25,19 +25,21 @@ db <- sd_database(
 )
 
 server <- function(input, output, session) {
+
+  # This is showcasing a map example
   # Interactive question with custom type
   sd_question(
     type   = "custom",
-    id     = "map_usa",
+    id     = "map_of_usa",
     label  = "Click on the state you live in:",
-    option = maps::map("state", plot = FALSE)$names
+    option = leafletOutput("usa_map", height = "400px")
   )
 
   # Reactive value storing state selection
   selected_state <- reactiveVal("(choose a state)")
 
   # Create the map
-  output$map_usa_map <- renderLeaflet({
+  output$usa_map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       setView(lng = -98.5795, lat = 39.8283, zoom = 4) %>%
@@ -54,21 +56,55 @@ server <- function(input, output, session) {
           fillOpacity = 0.7,
           bringToFront = TRUE
         ),
+        group = "states",  # Add a group name
         layerId = maps::map("state", plot = FALSE)$names
       )
   })
 
   # Click observer
-  observeEvent(input$map_usa_map_shape_click, {
-    state_name <- input$map_usa_map_shape_click$id
+  observeEvent(input$usa_map_shape_click, {
+    state_name <- input$usa_map_shape_click$id
     if (!is.null(state_name)) {
       state_name <- stringr::str_to_title(state_name)
       state_name <- stringr::str_replace(state_name, ':main', '')
       selected_state(state_name)
-      shiny::updateTextInput(session, "map_usa", value = state_name)
-      shiny::updateTextInput(session, "map_usa_interacted", value = TRUE)
+      shiny::updateTextInput(session, "map_of_usa", value = state_name)
+      shiny::updateTextInput(session, "map_of_usa_interacted", value = TRUE)
+
+      # Update map colors
+      leafletProxy("usa_map") %>%
+        addPolygons(
+          data = maps::map("state", plot = FALSE, fill = TRUE),
+          fillColor = ifelse(
+            maps::map("state", plot = FALSE)$names == input$usa_map_shape_click$id,
+            "orange",
+            "lightblue"
+          ),
+          weight = 2,
+          opacity = 1,
+          color = "white",
+          fillOpacity = 0.7,
+          highlightOptions = highlightOptions(
+            weight = 3,
+            color = "#666",
+            fillOpacity = 0.7,
+            bringToFront = TRUE
+          ),
+          layerId = maps::map("state", plot = FALSE)$names
+        )
     }
   })
+
+  # For a generalized custom question type, use this basic structure:
+  # sd_question(
+  #   type = "custom",
+  #   id = "custom_input",
+  #   label = "Some custom input:",
+  #   option = tags$div(
+  #     class = "my-custom-input",
+  #     "Custom content here"
+  #   )
+  # )
 
   # Database designation and other settings
   sd_server(
